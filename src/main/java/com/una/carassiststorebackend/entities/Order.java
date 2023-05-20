1f1e301e0ddeb13f6532ba8ab37f99f7af19f617
henriquewallace <henrique.wallace@hotmail.com>
2023-05-19 23:44:27 -0300

    Create constructor, getters and setters to Order class

--- src/main/java/com/una/carassiststorebackend/entities/Order.java
+++ src/main/java/com/una/carassiststorebackend/entities/Order.java
@@ -1,4 +1,61 @@
 package com.una.carassiststorebackend.entities;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Order {
+    private String id;
+    private String productId;
+    private BigDecimal totalPrice;
+    private Date date;
+    private String paymentType;
+
+    public Order() { super(); }
+
+    public Order(BigDecimal totalPrice, Date date, String paymentType) {
+        super();
+        this.totalPrice = totalPrice;
+        this.date = date;
+        this.paymentType = paymentType;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return this.productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return this.totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getPaymentType() {
+        return this.paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
 }
